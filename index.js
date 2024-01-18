ac29dae5841bc0ba8312cabb067da26cb0e6fb07
tanu241 <mehartanvi958@gmail.com>
2024-01-18 18:50:43 +0530

    hey

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('hello')
+
+
+//var vs let vs const 
+//var :- redefine
+//let :- not redifine 
+//const :- 
+
+//var a=5
+//var a=10
+
+//let a= 10
+//let b= 20
+
+//const c = 10 
+//console.log(c) 
+
+//let fristname='abc'
+
+//let fristName='abc'
+//let lastName='xyz'
+
+//console log('fristName + lastName')
+
+
+
+
+// = vs == vs ===
+
+//console log (5=='5')
+//console log (5==='5')
+
+
+
+
